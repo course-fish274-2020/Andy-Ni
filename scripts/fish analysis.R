f975c10624a967743cad214b038e8d7625636c96
ManCandyAndy <niandy89@gmail.com>
2020-11-10 10:33:10 -0800

    A comment was made about fish over 300

--- scripts/fish analysis.R
+++ scripts/fish analysis.R
@@ -1,7 +1,11 @@
 
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
+
 library(dplyr)
+
+# adding an additional column that classifies fish over 300m
+
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
